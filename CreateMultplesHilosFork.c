8521e52851d671b4d240ad512b1e08cc01544a87
Robert <48912628+robertfox11@users.noreply.github.com>
2023-03-19 11:01:22 +0100

    crea múltiples procesos hijos con la función fork
    El código es un programa que crea múltiples procesos hijos utilizando la función fork() y muestra el ID del padre y del hijo para cada proceso. El número de procesos hijos que se crean se especifica en el primer argumento de línea de comandos.

---
+++ CreateMultplesHilosFork.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int i, n;
+    pid_t hijopid;
+    if (argc != 2) {
+        fprintf(stderr, "USO: %s num_procesos\n", argv[0]);
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    for (i = 1; i < n; i++) {
+        hijopid = fork();
+        if (hijopid == 0) // Si es el hijo
+            break;
+        else if (hijopid == -1) { // Si hay un error
+            perror("error al ejecutar el fork");
+            exit(1);
+        }
+    }
+    fprintf(stdout, "proceso i:%d - ID del padre: %ld - ID del hijo: %ld \n", i, (long)getppid(), (long)getpid());
+    exit(0);
+}
